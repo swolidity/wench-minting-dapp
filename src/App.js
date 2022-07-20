dfaa41ee8e2c3d84d4b574672d49d69de438f13c
Andy Kay <andykay.me@gmail.com>
2022-07-20 09:00:22 -0400

    initial wench

--- src/App.js
+++ src/App.js
@@ -76,7 +76,6 @@ export const StyledLogo = styled.img`
 
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  border: 4px dashed var(--secondary);
   background-color: var(--accent);
   border-radius: 100%;
   width: 200px;
@@ -162,8 +161,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 10) {
-      newMintAmount = 10;
+    if (newMintAmount > 20) {
+      newMintAmount = 20;
     }
     setMintAmount(newMintAmount);
   };
@@ -201,7 +200,7 @@ function App() {
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
-        <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+        <StyledLogo alt={"logo"} src={"/config/images/logo.jpg"} />
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
@@ -216,7 +215,6 @@ function App() {
               backgroundColor: "var(--accent)",
               padding: 24,
               borderRadius: 24,
-              border: "4px dashed var(--secondary)",
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
           >
@@ -386,6 +384,88 @@ function App() {
               color: "var(--primary-text)",
             }}
           >
+            <div>
+              <h1>Wench 2043 FAQ</h1>
+            </div>
+            <div>
+              <h2>What is Wench 2043?</h2>
+              Wench 2043 is a collection of 2,043 hand made NFT's with the most
+              horrifically beautiful Wenches' living on the Ethereum Blockchain
+              Brothel. No two are alike, with 195 unique traits over 10 layers,
+              the possible combinations are over 32 billion!
+            </div>
+            <div>
+              <h2>What are the Wenches made of?</h2>
+              All Wench's are handmade and programmatically generated based on
+              195 different assets such as background, body type, lips, eyes,
+              necklaces, hats/hair, and extras. For @wench2043 they are using
+              IPFS which relies on a blockchain-like-system to store and
+              distribute information, so all your cloned Wenches will be safe
+              Pirate Bay NFT.
+            </div>
+            <div>
+              <h2>How much will it cost to clone a Wench?</h2>
+              We have finalized our cloning price at 0.0069 ETH + gas to clone
+              your wench.
+            </div>
+            <div>
+              <h2>How many Wenches can I clone?</h2>
+              An individual can clone as many Wench's as they like, 20 at a
+              time.
+            </div>
+            <div>
+              <h2>How can I clone these Wench's?</h2>
+              Wench's can be cloned on our official website for the initial
+              release, https://wench2043.com. As they are cloned, you will be
+              able to trade them at http://Opensea.io. You will also be able to
+              participate in community giveaways to have a chance of winning
+              prized undercover mints.
+            </div>
+            <div>
+              <h2>When will I get my Wench after the mint?</h2>
+              Your Wench will be sent to your wallet right after the minting
+              which might take few minutes.
+            </div>
+            <div>
+              <h2>What Ethereum wallets can I use?</h2>
+              We plan to have Metamask.
+            </div>
+            <div>
+              <h2>When is the launch?</h2>
+              We plan on launching on or about early August 2022.Check our
+              Twitter @wench2043 for updated information.
+            </div>
+            <div>
+              <h2>Will you list on http://rarity.tools?</h2>
+              Yes, once all Wenches have been sold we we will list on rarity
+              sniper and http://rarity.tools asap.
+            </div>
+            <div>
+              <h2>What are the benefits of owning a Wench?</h2>
+              Besides getting full commercial use rights and IP of your Wench,
+              and their own store at http://Maddies.com to market their Wench
+              and keep profits, Owners are entitled to a membership that will
+              provide them with special perks & number of exclusive benefits for
+              Wench owners.
+            </div>
+            <div>
+              <h2>What lies ahead for Wench 2043?</h2>
+              Our goal is to create value for our holders by growing an awesome
+              community. Besides, we are involved in many different projects to
+              be able to give amazing (NFT) giveaways but also to develop, and
+              look into launch and implement Wench Staking! Our dev team has the
+              knowledge and experience to realize about anything.
+            </div>
+            <div>
+              <h2>Who are you?</h2>I am a self taught artist that decided to get
+              back to my art roots, after 30 years in the corporate world to
+              bring the Wench 2043 project to life!
+            </div>
+            <div>
+              <h2>How do we contact you?</h2>
+              In addition to the Twitter & Discord links below, you can email us
+              at wench@wench2043.com.
+            </div>
             Please make sure you are connected to the right network (
             {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
             Once you make the purchase, you cannot undo this action.
